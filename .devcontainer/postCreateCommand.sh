f482b9ce043587033d2ba9fa0e6bbb94d0c6e9b8
Sam Lin <samlin0508@gmail.com>
2024-05-24 01:15:28 +0000

    Add Jupyter Notebook.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,3 +3,4 @@ npm install -g @angular/cli@17.3.7
 sudo apt-get update
 sudo apt-get install -y python3-opencv 
 pip install numpy tensorflow==2.16.1 opencv-python scikit-image
+pip install notebook
