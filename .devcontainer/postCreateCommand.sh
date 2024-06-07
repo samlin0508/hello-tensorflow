6d82dddae389cd582948338968168d5d1eb22744
Sam Lin <samlin0508@gmail.com>
2024-06-07 16:07:26 +0800

    Add another LSTM example.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,14 @@ npm install -g @angular/cli@17.3.7
 
 sudo apt-get update
 sudo apt-get install -y python3-opencv 
-pip install numpy tensorflow==2.16.1 opencv-python scikit-image
-pip install notebook matplotlib psycopg2-binary pandas scikit-learn
+
+pip install opencv-python 
+pip install numpy 
+pip install scikit-image
+pip install matplotlib 
+pip install pandas 
+pip install scikit-learn 
+pip install label-studio
+pip install notebook
+pip install psycopg2-binary
+pip install tensorflow==2.16.1 
